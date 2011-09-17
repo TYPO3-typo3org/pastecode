07094287eda134e35ce04540619076994bdd2cf8
sven.burkert <sven.burkert@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2011-09-17 15:02:07 +0000

    something went wrong, completely re-add sources

--- extensions/projects/t3org/pastecode/trunk/ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined ('TYPO3_MODE')) {
-	die('Access denied.');
-}
-t3lib_extMgm::addUserTSConfig('options.saveDocNew.tx_pastecode_code=1');
-
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_pastecode_pi1.php', '_pi1', 'list_type', 1);
-t3lib_extMgm::addPItoST43($_EXTKEY, 'pi2/class.tx_pastecode_pi2.php', '_pi2', 'list_type', 0);
-?>
